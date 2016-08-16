7ae5843b2ef1559212d8fa2780174819e3b2df14
shahryar <s.shahryar75@gmail.com>
2016-08-17 03:26:01 +0430

    first commit

---
+++ freight.py
@@ -0,0 +1,19 @@
+__author__ = 'shahryar_slg'
+
+class freight :
+    """
+    later , other classes will inherit from this class
+    classes like : grocery , furnitcher , vehicle , etc.
+    """
+    def __init__(self, name, weight, destination, pickup_address, dimension, price, customer_name, receiver_name, id):
+        self.name=name
+        self.weight=weight
+        self.destination=destination
+        self.pickup_address=pickup_address
+        self.dimension=dimension
+        self.price=price
+        self.customer_name=customer_name
+        self.id=id
+        self.receiver_name=receiver_name
+
+    
