f4319ce76c7e3139b567a55f63638276655f8612
shahryar <s.shahryar75@gmail.com>
2016-08-25 03:18:37 +0430

    this file is used to do the initialization of lookup tables in the database

--- initialize_db.py
+++ initialize_db.py
@@ -1,1 +1,21 @@
-__author__ = 'panizava'
+__author__ = 'shahryar_slg'
+
+def init_db():
+    """
+    this function is responsible for initializing
+    the lookup tables in the database
+    :return: nothing!
+    """
+    from models import role
+    from routes import db
+
+    #fedding database the roles :
+    customer = role(title='customer')
+    db.session.add(customer)
+    db.session.commit()
+
+    courier = role(title='courier')
+    db.session.add(courier)
+    db.session.commit()
+
+
