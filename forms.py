71a6854be8e91af5e7c13613f67e5914c01441dc
shahryar <s.shahryar75@gmail.com>
2016-08-27 10:05:28 +0430

    big in validationg the form is fixed!

--- forms.py
+++ forms.py
@@ -13,14 +13,16 @@ class SignupForm(Form):
                                 validators=[DataRequired(message="pls , enter your phone number")])
 
     email = StringField('Email',
-                        validators=[DataRequired(message="email is required!"), Email(message="this is not an Email!!")])
+                        validators=[DataRequired(message="email is required!"),
+                                    Email(message="this is not an Email!!")])
 
     password = PasswordField('Password',
                              validators=[
                                  DataRequired(
-                                     message="Really?? Could you imagine a sign up without a password provided??")])
+                                     message="Really?? Could you imagine a sign up without a password provided??"),
+                                 EqualTo(fieldname='repeat_password', message="passwords must match")])
 
     repeat_password = PasswordField('Repeat Password',
-                                    validators=[EqualTo(password, message="password does NOT match")])
+                                    validators=[DataRequired(message='should not be empty!')])
 
     submit = SubmitField('Sign up')
