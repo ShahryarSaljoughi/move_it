3a10efb2c05f9cdde5c11aea4d80714b7cdf5342
shahryar <s.shahryar75@gmail.com>
2016-08-29 02:20:15 +0430

    bug fixed : the database dir was not defined properly .

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ import os
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + app.root_path+'\\database\\shipment.db'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, '\\database\\shipment.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, '\\app\\database\\shipment.db')
 
 """
 since , the server is intended to be RESTfull , there should not be any session ! hence I'm not setting the secret_key
