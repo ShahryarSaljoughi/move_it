05938cd686a368a5fce1e9ce839c6a00de298a64
shahryar <s.shahryar75@gmail.com>
2016-09-16 03:35:47 +0430

    date columns are added to freights table and pictures tabled

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ __author__ = 'shahryar_slg'
 
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as jsonSerializer, SignatureExpired, BadSignature
+from datetime import datetime
 
 from . import db
 from . import app
@@ -148,6 +149,7 @@ class Freight(db.Model):
     owner = db.Column(db.INTEGER, db.ForeignKey('users.id'))
     description = db.Column(db.TEXT)
     pictures = db.relationship('FreightPicture')
+    creation_data = db.Column(db.DateTime, default=datetime.utcnow())
 
     def __repr__(self):
         return "freight: owner : " + \
@@ -235,3 +237,4 @@ class FreightPicture(db.Model):
     path = db.Column(db.TEXT, nullable=False)
     freight_id = db.Column(db.INTEGER, db.ForeignKey('freights.id'))
     id = db.Column(db.INTEGER, primary_key=True)
+    created = db.Column(db.DateTime, default=datetime.utcnow())
