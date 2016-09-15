9a35ccf2e6a08655bf09eab769add3ce1a504634
shahryar <s.shahryar75@gmail.com>
2016-09-16 01:23:22 +0430

    path for saving pictures configured

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ import os
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + app.root_path+'\\database\\shipment.db'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, '\\app\\database\\shipment.db')
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///E:\\flask_example\\move_it\\app\\database\\shipment.db'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + BASE_DIR + '\\app\\database\\shipment.db'
@@ -21,7 +22,6 @@ THREADS_PER_PAGE = 2
 SECRET_KEY = "scared rabbits fly"
 
 # flask_mail configuration:
-
 MAIL_SERVER = 'smtp.googlemail.com'
 MAIL_PORT = 465
 MAIL_USE_TLS = False
@@ -30,3 +30,7 @@ ADMINS = ['s.shahryar75@gmail.com']
 MAIL_USERNAME = 's.shahryar75'
 MAIL_PASSWORD = 'meajcveaxrnvwvhy'
 MAIL_DEFAULT_SENDER = ('SHIPMENT TEAM', 's.shahryar75@gmail.com')
+
+# image uploading configuration
+FREIGHT_PICTURES_DIR = BASE_DIR + "\\media\\freight_pictures"
+ALLOWED_PICTURE_EXTENSIONS = ['jpg', 'jpeg', 'png']
