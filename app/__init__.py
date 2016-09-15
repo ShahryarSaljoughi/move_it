0a8034dea8b2f553e87e6f535010059abea228f1
shahryar <s.shahryar75@gmail.com>
2016-09-16 03:34:54 +0430

    using migrations

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,15 @@ __author__ = 'shahryar_slg'
 from flask import Flask
 # import os
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object('config')
 
 # note that by moving the next code to the line after registering the blueprint ,
 # we will get an error , because db is needed in the main package
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db, directory=app.config['MIGRATIONS_DIR'])
 
 # import blueprints
 from .main import main as main_blue_print
