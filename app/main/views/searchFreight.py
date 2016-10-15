f400bdc033ed2a537f1eef3026c1f756339168ed
shahryar_slg <s.shahryar75@gmail.com>
2016-10-15 10:47:30 +0330

    search is done! the problem is serialization!

--- app/main/views/searchFreight.py
+++ app/main/views/searchFreight.py
@@ -1,5 +1,6 @@
 __author__ = 'shahryar_saljoughi'
 
+from flask import render_template
 from flask import request, jsonify
 from datetime import datetime
 from pytz import timezone
@@ -9,19 +10,27 @@ from . import auth
 
 tehran = timezone('Asia/Tehran')
 
+
+def search_by_date(records, start, end):
+    records = records.filter(Freight.creation_data > start).filter(Freight.creation_data < end)
+
+
 # currently I'm just gonna implement searching by date. later other features will be added.
-@main.route('/search_frights')
+# the below method is just about validation of data and responding in the case we have a bad request!
+#  the core of searching is done by other methods!
+@main.route('/search_frights', methods=['POST'])
 def search_freights():
 
-    result = Freight.query.all()
-    # any iteration of the below loop will eliminate some records form result!
+    result = Freight.query
 
+    # each iteration of the below loop will eliminate some records form result!
     search_fields = request.json['by']
+
     for search_field in search_fields:
         if search_field == 'date':
 
             # the main search algorithm is done in another method. called search_by_date()
-            # here I will just get\validate requires datas and send them to that method.
+            # here I will just get\validate required data and send them to that method.
 
             if 'date' not in request.json:
                 return jsonify({'status': 'failure',
@@ -35,10 +44,14 @@ def search_freights():
                     'message': "'BAD REQUEST' : beginning of the period is not defined"
                 }), 400
 
+            # checks if start is a dictionary not an integer or what ever rather than dictionary
+            if not isinstance(start, dict):
+                return jsonify({"status": "failure", "message": "BAD REQUEST : start must be of type dictionary"})
+
             # needed keys for start time
             needed_keys = ['year', 'month', 'day']
             for item in needed_keys:
-                # CHECK IF ALL NEEDED KEYS ARE SENT
+                # CHECK IF ALL NEEDED KEYS ARE PROVIDED
                 if item not in start:
                     return jsonify({
                         'status': "failure",
@@ -47,7 +60,8 @@ def search_freights():
                 # TYPE VALIDATION
                 if not isinstance(start[item], int):
                     return jsonify({'status': 'failure',
-                                    'message': 'BAD REQUEST: all values of start key must be of type integer!'})
+                                    'message': 'BAD REQUEST: all values of start key (day,month,year)'
+                                               ' must be of type integer!'})
 
             # default value of hour,minute and second is zero!
             start = datetime(
@@ -63,13 +77,13 @@ def search_freights():
             # if any parameter is not sent i will use the current time by default!
             end = datetime(
                 year=dates["end"]["year"]
-                if "end" in dates and "year" in dates['end'] else
+                if "end" in dates and "year" in dates['end'] and isinstance(dates["end"]["year"], int) else
                 datetime.now(tehran).year,
                 month=dates["end"]["month"]
-                if "end" in dates and "month" in dates['end'] else
+                if "end" in dates and "month" in dates['end'] and isinstance(dates["end"]["month"], int) else
                 datetime.now(tehran).month,
                 day=dates["end"]["day"]
-                if "end" in dates and "day" in dates['end'] else
+                if "end" in dates and "day" in dates['end'] and isinstance(dates["end"]["day"], int) else
                 datetime.now(tehran).day,
                 hour=dates["end"]["hour"]
                 if "end" in dates and "hour" in dates['end'] else 23,
@@ -78,7 +92,8 @@ def search_freights():
             )
 
             search_by_date(result, start, end)
+    print result
+    return jsonify({'status': "success", "result": result.all()}), 200
 
 
-def search_by_date(records, start, end):
-    pass
+
