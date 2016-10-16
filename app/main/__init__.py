262c6af62aa80b23f9c88f9c2693c13e60561744
shahryar_slg <s.shahryar75@gmail.com>
2016-10-16 10:43:56 +0330

    imports updated! I splitted the views into 3 files, hence, 3 imports are added to __init__.py

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -5,4 +5,5 @@ from flask import Blueprint
 
 main = Blueprint('main', __name__)
 
-from app.main.views import views
+from app.main.views import searchFreight
+from app.main.views import views_general, freightCRUD, registration
