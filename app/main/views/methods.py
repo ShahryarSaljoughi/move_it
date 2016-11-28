bce26e8b1203031105de4a750b0f95fcccfc3e8e
shahryar_slg <s.shahryar75@gmail.com>
2016-11-29 00:27:27 +0330

    the url written in sent mails updated! (since the server has changed)

--- app/main/views/methods.py
+++ app/main/views/methods.py
@@ -26,7 +26,7 @@ def send_confirmation_email(email, name, token):
     msg = Message(subject='SHIPMENT-confirm your email',
                   recipients=[email])
     # url = 'http://localhost:5000/email_confirmation/'+str(token)
-    url = 'http://192.99.103.124:9000/email_confirmation/'+str(token)
+    url = 'http://136.243.203.173:9000/email_confirmation/'+str(token)
     msg.body = "{}".format(url)
     mail.send(msg)
 
