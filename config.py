ef90e1456897a8fdf9a37a781e0536f7a7161251
Shahryar Saljoughi <s.shahryar75@gmail.com>
2016-12-14 02:51:30 +0330

    config.py : freight_picture_dir updated
    there is a problem in uploading images . 
    I guess It's caused by differences between Windows and Linux!

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ MAIL_PASSWORD = 'meajcveaxrnvwvhy'
 MAIL_DEFAULT_SENDER = ('SHIPMENT TEAM', 's.shahryar75@gmail.com')
 
 # image uploading configuration
-FREIGHT_PICTURES_DIR = BASE_DIR + "\\media\\freight_pictures"
+FREIGHT_PICTURES_DIR = BASE_DIR + "/media/freight_pictures"
 ALLOWED_PICTURE_EXTENSIONS = ['jpg', 'jpeg', 'png']
 
 MIGRATIONS_DIR = BASE_DIR + '\\app\\database\\migrations'  # os.path.join(BASE_DIR, '\\app\\database\\migrations')
