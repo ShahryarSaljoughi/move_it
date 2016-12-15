327a097702e598f7471ae0b115b75c34ba9b7637
shahryar_slg <s.shahryar75@gmail.com>
2016-12-15 16:39:33 +0330

    error handlers implemented

--- app/main/views/views.py
+++ app/main/views/views.py
@@ -1,8 +1,9 @@
 
 from flask import render_template, make_response, jsonify
 from app.main import main
-
-
+from flask import request
+from app import app
+from . import auth
 
 @main.route('/author')
 def see_author():
@@ -15,9 +16,27 @@ def hello_world():
     return render_template('main_page.html')
 
 
-@main.errorhandler(404)
-def not_found():
-    return make_response(jsonify({'error': 'Not found'}), 404)
+@app.errorhandler(404)
+def not_found(exception):
+    return make_response(jsonify({'error': 'Not found -404'}), 404)
+
+
+@app.errorhandler(401)
+def unauthorized_access(error):
+    return jsonify("unauthorized access")
+
+
+####################################
+# @auth.error_handler
+# def auth_error():
+#     """
+#     this function will response to client, regarding the fact that the client is not authenticated!
+#     :return:
+#     """
+#     # following if else blocks , indicate which view function caused error
+#     if "{}/testy".format(app.config['PORT']) in request.url and "GET" == request.method:
+#         pass
+####################################
 
 
 # if __name__ == '__main__':
