6b013bb00a254e025eebd837e2f5215e692f7c0a
shahryar_slg <s.shahryar75@gmail.com>
2017-01-31 13:16:43 +0330

    mailingError implemented

---
+++ app/main/appExceptions.py
@@ -0,0 +1,19 @@
+
+
+class MailingError(Exception):
+    """
+    this exception is raised in case sending confirmation mail fails
+    """
+    status_code = 501
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
