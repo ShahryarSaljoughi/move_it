c9ae0f731495567fb489d8462a1205f636c22cc7
shahryar_slg <s.shahryar75@gmail.com>
2017-02-05 19:35:31 +0330

    config ...

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///E:\\flask_example\\move_it\\app\\database\\shipment.db'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + BASE_DIR + '\\app\\database\\shipment.db'
 # SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, '\\app\\database\\db_repository')
-
+VPS = '78.47.195.58'
 PORT = 5000
 HOST = '0.0.0.0'
 THREADS_PER_PAGE = 2
