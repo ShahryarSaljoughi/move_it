7c2c4272a66c0f4f60c464033e2a6dd63936cf68
shahryar_slg <s.shahryar75@gmail.com>
2017-02-10 21:34:05 +0330

    new schema : freight_received implemented

--- app/main/validation/schemas.py
+++ app/main/validation/schemas.py
@@ -2,5 +2,11 @@
 # Tender schemas **************************************
 
 show_tenders = {
-    'freight_id': {'type': 'integer', 'required': True}
+    'freight_id': {'type': 'integer', 'required': True, 'min': 1}
 }
+
+freight_received = {
+    'tender_id': {'type': 'integer', 'required': True, 'min': 1}
+}
+
+# End of Tender schemas ********************************
