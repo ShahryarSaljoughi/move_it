f355b88ef86b57c52134ccbf092bd38decc9a29e
shahryar_slg <s.shahryar75@gmail.com>
2017-02-13 11:46:45 +0330

    schema for approve_courier implemented

--- app/main/validation/schemas.py
+++ app/main/validation/schemas.py
@@ -9,4 +9,7 @@ freight_received = {
     'tender_id': {'type': 'integer', 'required': True, 'min': 1}
 }
 
+approve_courier = {
+    'tender_id': {'type': 'integer', 'required': True, 'min': 1}
+}
 # End of Tender schemas ********************************
