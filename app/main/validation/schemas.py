978ecb1816aec040bafab092f2e1d477b00e4682
shahryar_slg <s.shahryar75@gmail.com>
2017-02-13 13:23:57 +0330

    schema.apply_freight implemented

--- app/main/validation/schemas.py
+++ app/main/validation/schemas.py
@@ -12,4 +12,10 @@ freight_received = {
 approve_courier = {
     'tender_id': {'type': 'integer', 'required': True, 'min': 1}
 }
+
+apply_freight = {
+    'freight_id': {'type': 'integer', 'required': True, 'min': 1},
+    'price': {'type': 'number', 'required': True, 'min': 0},
+    'description': {'type': 'string', 'required': False, 'maxlength': 3500}
+}
 # End of Tender schemas ********************************
