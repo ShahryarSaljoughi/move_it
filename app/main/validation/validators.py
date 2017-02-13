90e47720f2fa1955f056f4abdd9ba9fbd39fd1ca
shahryar_slg <s.shahryar75@gmail.com>
2017-02-13 11:27:12 +0330

    validate_freight_received is improved!

--- app/main/validation/validators.py
+++ app/main/validation/validators.py
@@ -20,13 +20,14 @@ def validate_show_tender(document, result, validator):
     validates data associated with the view function: TenderCore.show_tender
     :param document: request.json
     :param result: result is a dict to be modified!
+    :param validator: validator is an instance of cerberus.Validator(schema) with the proper schema!
     """
     is_doc_validated = result['is_validated']
-    errors = validator.errors  # so, errors is a dict!
+    errors = result['errors']  # so, errors is a dict!
 
     # check if freight id is in database
     if is_doc_validated and not Freight.query.get(document['freight_id']):
-        assert 'freight_id' not in v.errors.keys()
+        assert 'freight_id' not in errors.keys()
         errors['freight_id'] = list()
         errors['freight_id'].append('freight_id does not found')
         is_doc_validated = False
@@ -35,33 +36,33 @@ def validate_show_tender(document, result, validator):
     result['errors'] = errors
 
 
-def validate_freight_received(document):
+def validate_freight_received(document, result, validator):
     """
     validates data associated with the view function: TenderCore.freight_received
     :param document: request.json
-    :return: result , result is a dictionary, containing keys 'is_validated' and 'errors'
+    :param result: , result is a dictionary, containing keys: 'is_validated' and 'errors'
+    :param validator: an instance of cerberus.Validator . validator.schema is already set.
     """
-    v = Validator(schema=schemas.freight_received)
-    result = dict()
-    is_doc_validated = v.validate(document)
-    errors = v.errors
+
+    is_doc_validated = result['is_validated']
+    errors = result['errors']
 
     # check if tender exists in database:
     tender = Tender.query.get(document['tender_id'])  # tender of None
-    if is_doc_validated and tender is None:
-        assert 'tender_id' not in v.errors.keys()  # because doc is validated so far!
-        errors['tender_id'] = ['tender_id not found!']
-        is_doc_validated = False
-
-    # only the owner of the freight can approve it's received! Let's check it:
     if is_doc_validated:
+        if tender is None:
+            assert 'tender_id' not in errors.keys()  # because doc is validated so far!
+            errors['tender_id'] = ['tender_id not found!']
+            is_doc_validated = False
+
+        # only the owner of the freight can approve it's received! Let's check it:
         if g.user != Tender.query.get(document['tender_id']).freight.owner:
             errors['access denied'] = 'only the owner of the freight can approve it is delivered'
             is_doc_validated = False
 
+
     result['is_validated'] = is_doc_validated
     result['errors'] = errors
 
-    return result
 
-def validate_approve_courier()
+def validate_approve_courier(document, result):
