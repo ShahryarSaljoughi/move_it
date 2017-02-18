5076906ee45e20f49d4a733fb31d0d739f61c881
shahryar_slg <s.shahryar75@gmail.com>
2017-02-18 17:08:35 +0330

    new rule is used in schemas

--- app/main/validation/schemas.py
+++ app/main/validation/schemas.py
@@ -2,31 +2,33 @@
 
 # custom rule for validators:
 def id_found(field, value, error):
+
+    cls = field[:len(field)-3].title()  # freight_id --> Freight
     try:
-        cls = field[:len(field)-3].title()  # freight_id --> Freight
         data = eval('{}.query.get({})'.format(cls, field))
         if data is None:
             error(field, "{} not found".format(field))
 
-    except:
-        error(field, "{} is not valid".format(field))
+    except NameError:
+        # some class names can't be reached via the approach I designed ! these class names are handled here:
+        pass  # todo: associate field with class names!
 
 # Tender schemas **************************************
 
 show_tenders = {
-    'freight_id': {'type': 'integer', 'required': True, 'min': 1}
+    'freight_id': {'type': 'integer', 'required': True, 'min': 1, 'validator': id_found}
 }
 
 freight_received = {
-    'tender_id': {'type': 'integer', 'required': True, 'min': 1}
+    'tender_id': {'type': 'integer', 'required': True, 'min': 1, 'validator': id_found}
 }
 
 approve_courier = {
-    'tender_id': {'type': 'integer', 'required': True, 'min': 1}
+    'tender_id': {'type': 'integer', 'required': True, 'min': 1, 'validator': id_found}
 }
 
 apply_freight = {
-    'freight_id': {'type': 'integer', 'required': True, 'min': 1},
+    'freight_id': {'type': 'integer', 'required': True, 'min': 1, 'validator': id_found},
     'price': {'type': 'number', 'required': True, 'min': 0},
     'description': {'type': 'string', 'required': False, 'maxlength': 3500}
 }
