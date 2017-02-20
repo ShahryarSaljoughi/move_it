6c0c6bc3dde06602d1ef1a32434bda4aeacfe140
shahryar_slg <s.shahryar75@gmail.com>
2017-02-20 10:27:56 +0330

    new rule added : is_courier! adds an error to v.errors if the the user is needed to be a courier but is not! (usefull in APIs which can be accessed only by couriers!)

--- app/main/validation/schemas.py
+++ app/main/validation/schemas.py
@@ -1,12 +1,12 @@
 from app.models import role
 
 
-# custom rule for validators:
+# custom rules for validators:
 def id_found(field, value, error):
 
     cls = field[:len(field)-3].title()  # freight_id --> Freight
     try:
-        data = eval('{}.query.get({})'.format(cls, field))
+        data = eval('{}.query.get({})'.format(cls, value))
         if data is None:
             error(field, "{} not found".format(field))
 
@@ -14,7 +14,14 @@ def id_found(field, value, error):
         # some class names can't be reached via the approach I designed ! these class names are handled here:
         pass  # todo: associate field with class names!
 
+
+def is_courier(field, value, error):
+    if value != role.query.get(2).title:
+        error(field, "only couriers can use this route!")
+
+
 # Tender schemas **************************************
+
 
 show_tenders = {
     'freight_id': {'type': 'integer', 'required': True, 'min': 1, 'validator': id_found}
@@ -32,7 +39,8 @@ apply_freight = {
     'freight_id': {'type': 'integer', 'required': True, 'min': 1, 'validator': id_found},
     'price': {'type': 'number', 'required': True, 'min': 0},
     'description': {'type': 'string', 'required': False, 'maxlength': 3500},
-    'user_type': {'allowed': [role.query.get(2).title],  # user_type : courier
-                  'required': True}
+    'user_type': {'allowed': [r.title for r in role.query.all()],  # user_type : [courier, customer]
+                  'required': True,                                # (other roles can be added later)
+                  'validator': is_courier}
 }
 # End of Tender schemas ********************************
