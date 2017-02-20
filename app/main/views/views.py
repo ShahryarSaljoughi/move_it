fa28296c99e61cee8e69dae4a9574d836a87dbf7
shahryar_slg <s.shahryar75@gmail.com>
2017-02-20 09:20:02 +0330

    before_request() implemented! I add some extra information (i.e user_id , role.title and etc.) to request.json! these information are used in validations! Consider the case in which the route is just provided for courieres! in this case it is enough to check the role.title provided in request.json!

--- app/main/views/views.py
+++ app/main/views/views.py
@@ -1,13 +1,33 @@
 
-from flask import render_template, make_response, jsonify
+from flask import render_template, make_response, jsonify, g
 from app.main import main, appExceptions
 from flask import request
 from app import app
+from app.main.views import  registration
 from . import auth
 
 @main.route('/author')
 def see_author():
     return render_template('aboutAuthor.html')
+
+
+@main.before_request
+def add_authorization_to_json():
+    # I want to add some more information to request.json before it is processed in views
+    # these extra information are used in validations!
+
+    if request.is_json:
+        try:
+            g_has_user = registration.verify_password(request.authorization['username'],
+                                                      request.authorization['password'])
+
+        # in case request.authorization is None:
+        except TypeError:
+            g_has_user = registration.verify_password('', '')
+
+        if g_has_user:
+            request.json['user_type'] = g.user.role.title
+            request.json['user_id'] = g.user.id
 
 
 # the below method is just for fun and can be deleted:
