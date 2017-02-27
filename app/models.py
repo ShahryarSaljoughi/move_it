a0be62918a22ac1aed09c13f0fe1edbdf57700ac
shahryar_slg <s.shahryar75@gmail.com>
2017-02-27 19:57:22 +0330

    tender_id

--- app/models.py
+++ app/models.py
@@ -361,6 +361,7 @@ class Tender(db.Model):
         return {
             '__type__': 'Tender',
             'freight_id': self.freight_id,
+            'tender_id' : self.id
             'courier_id': self.courier_id,
             'courier': self.courier,
             'price': self.suggested_price,
