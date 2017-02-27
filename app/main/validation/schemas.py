d2ec0767475a51a9d3530c127ee4cef102077dac
shahryar_slg <s.shahryar75@gmail.com>
2017-02-27 12:45:08 +0330

    a new validator, freight_not_taken(), is implemented to check if freight's courier is chosen or not.

--- app/main/validation/schemas.py
+++ app/main/validation/schemas.py
@@ -18,14 +18,20 @@ def id_found(field, value, error):
 
 def is_courier(field, value, error):
     if value != role.query.get(2).title:
-        error(field, "only couriers can use this route!")
+        error('other_errors', "only couriers can use this api!")
 
 
 def user_is_freight_owner(field, value, error):  # for checking user's permissions ...
     assert hasattr(request.json, 'freight_id')
     freight = Freight.query.get(request.json['freight_id'])
     if freight.owner.id != g.user:
-        error(field, "Only the owner of the freight is permitted")
+        error('other_errors', "Only the owner of the freight is permitted")
+
+
+def freight_not_taken(field, value, error):
+    freight = Freight.query.get(value)
+    if freight.is_courier_chosen:
+        error('other_errors', "this freight is already assigned to another courier")
 
 
 def user_is_tender_freight_owner(field, value, error):  # for checking user's permissions ...
@@ -33,7 +39,7 @@ def user_is_tender_freight_owner(field, value, error):  # for checking user's pe
     assert hasattr(request.json, 'tender_id')
     tender = Tender.query.get(request.json['tender_id'])
     if g.user != tender.freight.owner.id:  # g.user = request.json['user_id]
-        error(field, "Only the owner of the freight is permitted")
+        error('other_errors', "Only the owner of the freight is permitted")
 
 # TenderCore schemas **************************************
 
@@ -51,9 +57,9 @@ approve_courier = {
 }
 
 apply_freight = {
-    'freight_id': {'type': 'integer', 'required': True, 'min': 1, 'validator': id_found},
+    'freight_id': {'type': 'integer', 'required': True, 'min': 1, 'validator': [id_found, freight_not_taken]},
     'price': {'type': 'number', 'required': True, 'min': 0},
-    'description': {'type': 'string', 'required': False, 'maxlength': 3500},
+    'description': {'type': 'string', 'required': False, 'maxlength': 4000},
     'user_type': {'allowed': [r.title for r in role.query.all()],  # user_type : [courier, customer]
                   'required': True,                                # (other roles can be added later)
                   'validator': is_courier}
